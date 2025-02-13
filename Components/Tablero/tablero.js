2f77fa9ab727c6aec7c874c0195f1a478dd6d482
Fluffy McDork <hdvillatoro@gmial.com>
2025-02-13 13:40:09 -0600

    Se hace una animacion de cartas y se edita el 	-tablero.js 	-tablero.css

--- Components/Tablero/tablero.js
+++ Components/Tablero/tablero.js
@@ -4,7 +4,26 @@ function item(contenido){
     let div1 = document.createElement('div'); 
     div1.className = "carta";
     div1.textContent = contenido;
+
+    let front = document.createElement('div');
+    front.className = "Front";
+
+    let back = document.createElement('div');
+    back.className = "Back";
+    back.innerText = contenido;
+
+    div1.appendChild(front);
+    div1.appendChild(back);
+
+
+    div1.addEventListener("click", ()=>{
+        div1.classList.toggle("volteada");
+    });
+    
+
     return div1;
+
+
 }
 
 function cargarCartas(){
@@ -16,7 +35,7 @@ function cargarCartas(){
     });
     
     return div2;
-};
+}
 
 
 export{cargarCartas}
